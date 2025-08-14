0aea612848424a925d8501ae28b529b9bb0918de
Asha Latha <asha.latha@graymatter.co.in>
2025-08-14 15:14:45 +0530

    cors updated with clienturl

--- backend/routes/alerts.js
+++ backend/routes/alerts.js
@@ -13,8 +13,9 @@ router.get("/stream", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL || "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL );
   res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Vary", "Origin");
   res.flushHeaders();
 
   // Register this client
